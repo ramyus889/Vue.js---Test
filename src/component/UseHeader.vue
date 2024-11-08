911eb8d6738f09199182f71dcde5ca887cf1d49d
ramyus889 <ramyus889.com>
2024-11-08 11:14:48 +0300

    header

--- src/component/UseHeader.vue
+++ src/component/UseHeader.vue
@@ -1,16 +1,27 @@
 <script setup>
 import { routeUrl } from '../router/routes.js';
+import UseDrawer from './UseDrawer.vue';
 </script>
 
 <template>
-  <div class="flex gap-5 px-5 py-5 border-b border-gray-500">
-    <RouterLink :to="`${routeUrl[0].path}`">Home</RouterLink>
-    <RouterLink :to="`${routeUrl[1].path}`">RestApi</RouterLink>
-    <RouterLink :to="`${routeUrl[2].path}`">UserList</RouterLink>
-    <RouterLink :to="`${routeUrl[3].path}`">SearchList</RouterLink>
-    <RouterLink :to="`${routeUrl[4].path}`">DeleteUser</RouterLink>
-    <RouterLink :to="`${routeUrl[5].path}`">PushList</RouterLink>
-    <RouterLink :to="`${routeUrl[6].path}`">EditUser</RouterLink>
-    <RouterLink :to="`${routeUrl[7].path}`">GSAP</RouterLink>
+  <div
+    class="flex sm:flex-row flex-col justify-between items-center gap-5 min-[1800px]:mx-[400px] xl:mx-[200px] lg:mx-[100px] md:mx-[50px] md:px-0 px-5 sm:py-[35px]"
+  >
+    <div class="block sm:hidden">
+      <UseDrawer />
+    </div>
+    <div class="flex items-center w-full gap-5">
+      <RouterLink
+        :to="`${routeUrl[0].path}`"
+        class="py-2.5 sm:px-6 bg-[#FE5F5F] rounded-full text-white font-semibold text-center w-full sm:w-auto"
+        >Личный кабинет</RouterLink
+      >
+      <a href="#" class="hidden font-bold sm:block"> Каталог услуг </a>
+    </div>
+    <div class="hidden sm:block">
+      <a href="#">
+        <img src="/testFigma/Frame13.png" alt="" />
+      </a>
+    </div>
   </div>
 </template>
