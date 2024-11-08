00bff4bed96d7fc50f62ed9e6e852156fcd3741e
ramyus889 <ramyus889.com>
2024-11-08 14:07:54 +0300

    add

--- src/component/Main/UseMain.vue
+++ src/component/Main/UseMain.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="sticky top-0 left-0 right-0">
     <div
-      class="w-full h-screen bg-[url('/testFigma/Group.png')] bg-no-repeat -z-40 pt-[150px] bg-cover bg-center"
+      class="w-full h-screen bg-[url('/testFigma/Group.png')] bg-no-repeat -z-40 pt-[150px] bg-cover bg-bottom"
     >
       <div class="min-[1800px]:mx-[400px] xl:mx-[200px] lg:mx-[100px] md:mx-[50px]">as</div>
     </div>
