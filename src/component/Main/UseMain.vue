50103f8e95b97d8ce31a426fa3dd3558905087e4
ramyus889 <ramyus889.com>
2024-11-08 14:26:28 +0300

    add

--- src/component/Main/UseMain.vue
+++ src/component/Main/UseMain.vue
@@ -4,7 +4,16 @@
     <div
       class="w-full h-screen bg-[url('/testFigma/Group.png')] bg-no-repeat -z-40 pt-[150px] bg-cover bg-bottom"
     >
-      <div class="min-[1800px]:mx-[400px] xl:mx-[200px] lg:mx-[100px] md:mx-[50px]">as</div>
+      <div
+        class="min-[1800px]:mx-[400px] xl:mx-[200px] lg:mx-[100px] md:mx-[50px] md:px-0 px-5 flex flex-col max-[640px]:items-center gap-[70px]"
+      >
+        <img src="/testFigma/Group673.svg" class="w-[209px]" alt="" />
+        <div
+          class="max-w-[620px] font-bold md:text-[35px] sm:text-left text-[25px] text-center md:leading-[44px] leading-[34px]"
+        >
+          Мой бизнес - цифровая платформа для тех, кто хочет открыть и развивать свой бизнес
+        </div>
+      </div>
     </div>
   </div>
 </template>
